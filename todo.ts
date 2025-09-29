f331a655b61300a79e22a5bcd5be46665b296dc9
Hoareau.Steevy <shoareau@dev-id.fr>
2025-09-29 16:56:40 +0200

    delete old file

--- todo.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from './types/task.type';
-
-function addTodo(task: Task) {
-  if (!task.title) throw new Error('Title is required');
-  if (!task.id) throw new Error('Task id is required');
-
-  console.log(task);
-}
